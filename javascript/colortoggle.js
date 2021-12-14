675fdfc773b616091cc5cc145a660f04200aefb8
Tommy-Christafano <tommychristafano928@gmail.com>
2021-12-14 00:19:08 -0500

    purpose, team, javascript-partial

---
+++ javascript/colortoggle.js
@@ -0,0 +1,47 @@
+function init() {
+    var fieldset = document.getElementsByTagName('input');
+    for (var i = 0; i < fieldset.length; i++) {
+        fieldset[i].addEventListener('click', toggle, false);
+    }
+}
+
+function toggle() {
+    var id = this.id;
+    switch (id) {
+        case "wartoggle": {
+            var ref = document.getElementsByClassName("war");
+            for (var i = 0; i < ref.length; i++) {
+                ref[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "diplomacytoggle": {
+            var ref = document.getElementsByClassName("diplomacy");
+            for (var i = 0; i < ref.length; i++) {
+                ref[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "economytoggle": {
+            var ref = document.getElementsByClassName("economy");
+            for (var i = 0; i < ref.length; i++) {
+                ref[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "socialtoggle": {
+            var ref = document.getElementsByClassName("social");
+            for (var i = 0; i < ref.length; i++) {
+                ref[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "religiontoggle": {
+            var ref = document.getElementsByClassName("religion");
+            for (var i = 0; i < ref.length; i++) {
+                ref[i].classList.toggle("on")
+            }
+        };
+        break;
+    }
+  }
